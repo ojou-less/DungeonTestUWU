3dddf586c2868a2544ed5f289f2fab26685ae1b8
mark <mark.fischer0404@gmail.com>
2022-06-02 18:39:21 +0200

    bruv

--- src/main/java/de/prog2/uwu/Main.java
+++ src/main/java/de/prog2/uwu/Main.java
@@ -3,12 +3,18 @@ package de.prog2.uwu;
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
 import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.level.Level;
 import com.almasb.fxgl.entity.level.LevelLoader;
+import com.almasb.fxgl.input.UserAction;
 
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameWorld;
+import static com.almasb.fxgl.dsl.FXGLForKtKt.getInput;
+import static de.prog2.uwu.Type.*;
 
 public class Main extends GameApplication {
+
+    private Entity player;
 
     @Override
     protected void initSettings(GameSettings settings){
@@ -20,6 +26,23 @@ public class Main extends GameApplication {
     protected void initGame() {
         super.initGame();
         FXGL.getGameWorld().addEntityFactory(new Factory());
+        FXGL.setLevelFromMap("tmx/derDungeon.tmx");
+    }
+
+    @Override
+    protected void initInput(){
+        //FIXME: bisher echt kaputt die scheiße hilfe man!!!!
+//        FXGL.getInput().addAction(new UserAction("Interact") {
+//            @Override
+//            protected void onActionBegin() {
+//                getGameWorld().getEntitiesByType(INTERACTABLE)
+//                        .stream()
+//                        .filter(btn -> player.isColliding(btn))
+//                        .forEach(btn -> {
+//                            System.out.println("mit event interagiert");    //hier die aktion einbauen
+//                        });                                                 //in FXGL nur über hardcode möglich.
+//            }
+//        });
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/de/prog2/uwu/Type.java
@@ -0,0 +1,5 @@
+package de.prog2.uwu;
+
+public enum Type {
+    PLAYER, INTERACTABLE, MAP_BORDER, MAP_TILE
+}
